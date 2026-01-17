e904da04a6e27c0160c6ac37f180297b10db4f49
jaliriogbarrios19 <jaliriogbarrios@gmail.com>
2026-01-17 16:29:03 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,15 @@
 // Este archivo es necesario para que Chrome permita la instalación de la App
 const CACHE_NAME = 'bhai-calendar-v1';
-const assets = ['./', './index.html', './manifest.json'];
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/2693/2693630.png'
+];
 
+// Instalación: Guardar archivos en caché y forzar activación inmediata
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
@@ -10,10 +17,7 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+// Activación: Tomar el control de la página de inmediato
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
 });
