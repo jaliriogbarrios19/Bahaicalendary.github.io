a829c0c045fa00d6fd21e0d6caf77a07484efd78
jaliriogbarrios19 <jaliriogbarrios@gmail.com>
2026-02-14 14:45:16 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,11 @@
-// Este archivo permite que la App se instale y funcione mejor
-const CACHE_NAME = 'bhai-app-v5';
+const CACHE_NAME = 'bhai-app-v6';
 const assets = [
   './',
   './index.html',
   './manifest.json',
-  'https://cdn-icons-png.flaticon.com/512/2693/2693630.png'
+  '[https://cdn-icons-png.flaticon.com/512/2693/2693630.png](https://cdn-icons-png.flaticon.com/512/2693/2693630.png)'
 ];
 
-// Instalación: Guarda los archivos básicos en la memoria del celular
 self.addEventListener('install', e => {
   self.skipWaiting();
   e.waitUntil(
@@ -15,12 +13,10 @@ self.addEventListener('install', e => {
   );
 });
 
-// Activación: Toma control de la App inmediatamente
 self.addEventListener('activate', e => {
   e.waitUntil(clients.claim());
 });
 
-// Peticiones: Carga desde la memoria para que la App sea rápida
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request))
